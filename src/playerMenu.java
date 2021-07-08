a59fdbeb188af0bfceba20606f9434ca210aed4e
e-d-dyanfernando <85722967+e-d-dyanfernando@users.noreply.github.com>
2021-07-08 12:58:24 +0530

    Add files via upload

---
+++ src/playerMenu.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class playerMenu {
+
+    int count=0;
+
+    public void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter A to add a player:");
+        add();
+
+
+        System.out.println("Enter A to add a player:");
+        view();
+    }
+        public void view () {
+        }
+
+        public void add () {
+            //player p1=new player();
+            //count++;
+            //System.out.println("player added");
+
+        }
+
+
+            //count++;
+            //System.out.println("player added");
+
+}
+
+
